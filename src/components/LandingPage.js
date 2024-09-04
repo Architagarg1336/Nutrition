4926ba5e92a63cc27d634677e25d251446bd9993
anshudivu1 <divyanshusharma1204@gmail.com>
2024-09-04 22:16:43 +0530

    Changed Text and added button

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -8,14 +8,34 @@ function LandingPage() {
     <div className="landing-page">
       <div className="content">
         <div className="text-section">
-          <h1>Healthy Inside,<br/> Happy Outside. 
+          <h1>We care about<br/>your <span className='landing-page-slogan'> Health</span>. 
 
           </h1>
           
           {/* <p>Your description goes here. Add more text as needed to explain your content.</p> */}
           <div className='button-group'>
-          <button className='main-button'>Get started</button>
-          <button className='main-button'>Explore More</button>
+                    
+          <button class="customButton">
+            <div class="textContainer">
+              <span className ='Customize'>Customize Your Diet</span>
+              <span className ='feeling'>Feeling unsatisfied</span>
+            </div>
+            <div class="arrowIconContainer">
+              <svg
+                width="25"
+                height="25"
+                viewBox="0 0 45 38"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M43.7678 20.7678C44.7441 19.7915 44.7441 18.2085 43.7678 17.2322L27.8579 1.32233C26.8816 0.34602 25.2986 0.34602 24.3223 1.32233C23.346 2.29864 23.346 3.88155 24.3223 4.85786L38.4645 19L24.3223 33.1421C23.346 34.1184 23.346 35.7014 24.3223 36.6777C25.2986 37.654 26.8816 37.654 27.8579 36.6777L43.7678 20.7678ZM0 21.5L42 21.5V16.5L0 16.5L0 21.5Z"
+                  fill="black"
+                ></path>
+              </svg>
+            </div>
+          </button>
+          {/* <button className='main-button'>Explore More</button> */}
           </div>
           <div className = 'lemon-glass'>
             <img src = '/images/lemonGlass.svg' classname = 'lemon-glass-image-left'/>
