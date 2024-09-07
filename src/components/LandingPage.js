818614530546d83a3248b1d5cdbb1c0ffec64b32
Archita <archita1336.be22@chitkara.edu.in>
2024-09-07 19:06:07 +0530

    Your commit message here

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -12,31 +12,24 @@ function LandingPage() {
 
           </h1>
           
-          {/* <p>Your description goes here. Add more text as needed to explain your content.</p> */}
-          <div className='button-group'>
-                    
-          <button class="customButton">
-            <div class="textContainer">
-              <span className ='Customize'>Customize Your Diet</span>
-              <span className ='feeling'>Feeling unsatisfied</span>
-            </div>
-            <div class="arrowIconContainer">
-              <svg
-                width="25"
-                height="25"
-                viewBox="0 0 45 38"
-                fill="none"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  d="M43.7678 20.7678C44.7441 19.7915 44.7441 18.2085 43.7678 17.2322L27.8579 1.32233C26.8816 0.34602 25.2986 0.34602 24.3223 1.32233C23.346 2.29864 23.346 3.88155 24.3223 4.85786L38.4645 19L24.3223 33.1421C23.346 34.1184 23.346 35.7014 24.3223 36.6777C25.2986 37.654 26.8816 37.654 27.8579 36.6777L43.7678 20.7678ZM0 21.5L42 21.5V16.5L0 16.5L0 21.5Z"
-                  fill="black"
-                ></path>
-              </svg>
-            </div>
-          </button>
-          {/* <button className='main-button'>Explore More</button> */}
-          </div>
+          <div className="button-group">
+      <button
+        className="font-sans flex justify-center gap-2 items-center mx-auto shadow-xl text-lg bg-gray-50 backdrop-blur-md lg:font-semibold isolation-auto border-gray-50 before:absolute before:w-full before:transition-all before:duration-700 before:hover:w-full before:-left-full before:hover:left-0 before:rounded-full before:bg-green-700 hover:text-gray-50 before:-z-10 before:aspect-square before:hover:scale-150 before:hover:duration-700 relative z-10 px-4 py-2 overflow-hidden border-2 rounded-full group"
+        type="submit"
+      >
+        Explore
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 19"
+          className="w-8 h-8 justify-end group-hover:rotate-90 group-hover:bg-gray-50 text-gray-50 ease-linear duration-300 rounded-full border border-gray-700 group-hover:border-none p-2 rotate-45"
+        >
+          <path
+            className="fill-gray-800 group-hover:fill-gray-800"
+            d="M7 18C7 18.5523 7.44772 19 8 19C8.55228 19 9 18.5523 9 18H7ZM8.70711 0.292893C8.31658 -0.0976311 7.68342 -0.0976311 7.29289 0.292893L0.928932 6.65685C0.538408 7.04738 0.538408 7.68054 0.928932 8.07107C1.31946 8.46159 1.95262 8.46159 2.34315 8.07107L8 2.41421L13.6569 8.07107C14.0474 8.46159 14.6805 8.46159 15.0711 8.07107C15.4616 7.68054 15.4616 7.04738 15.0711 6.65685L8.70711 0.292893ZM9 18L9 1H7L7 18H9Z"
+          ></path>
+        </svg>
+      </button>
+    </div>
           <div className = 'lemon-glass'>
             <img src = '/images/lemonGlass.svg' classname = 'lemon-glass-image-left'/>
           </div>
